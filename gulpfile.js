3fdb5b708f473f6ac7327aff2272e2f378764b5b
Luke Simkins <luke@oseberg.io>
2015-07-02 21:47:11 -0500

    Basic build process.

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp = require('gulp');
+var fs = require('fs');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var _ = require('lodash');
+
+function compile(watch) {
+  var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babelify));
+ 
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+  }
+ 
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+ 
+  rebundle();
+}
+ 
+function watch() {
+  return compile(true);
+};
+ 
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+ 
+gulp.task('default', ['watch']);
